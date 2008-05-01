45fbee1f9772205ea6eb0692099be231b7bfe1fe
herrsteiner <herrsteiner@a4770fbb-0739-0410-ac47-c227fb5b275d>
2008-05-01 01:35:38 +0000

    corrected the presetinstallerscript
    git-svn-id: https://minicomputer.svn.sourceforge.net/svnroot/minicomputer/trunk@87 a4770fbb-0739-0410-ac47-c227fb5b275d

--- installpresets.sh
+++ installpresets.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-if [! -d "${$HOME}/.miniComputer"];then
+if [ ! -d "${HOME}/.miniComputer" ];then
 	mkdir "${HOME}/.miniComputer"
 fi
-if [-e "${HOME}/.miniComputer/minicomputerMemory.txt"]
+if [ -e "${HOME}/.miniComputer/minicomputerMemory.txt" ];then
 	mv "${HOME}/.miniComputer/minicomputerMemory.txt" "${HOME}/.miniComputer/minicomputerMemory.txt.beforeInstall"
 	echo "backed up ${HOME}/.miniComputer/minicomputerMemory.txt"
 fi 
-if [-e "${HOME}/.miniComputer/minicomputerMulti.txt"]
+if [ -e "${HOME}/.miniComputer/minicomputerMulti.txt" ];then
 	mv "${HOME}/.miniComputer/minicomputerMulti.txt" "${HOME}/.miniComputer/minicomputerMulti.txt.beforeInstall"
 	echo "backed up ${HOME}/.miniComputer/minicomputerMulti.txt"
 fi 
